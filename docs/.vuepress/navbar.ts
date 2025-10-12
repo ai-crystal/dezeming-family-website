8cfe3ff21a6b92a3fee8470b6299444e05b72e36
threekd <threekd42@gmail.com>
2025-10-12 19:45:20 +0800

    update

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -16,6 +16,10 @@ export default defineNavbarConfig([
     items: [
       { text: '计算机系统与程序设计', link: '/dezeming/计算机系统与程序设计/计算机系统与程序设计.md' },
       { text: '人工智能与机器学习', link: '/dezeming/人工智能与机器学习/人工智能与机器学习.md' },
+      { text: '数据结构与算法', link: '/dezeming/数据结构与算法/数据结构与算法.md' },
+      { text: '应用数学原理', link: '/dezeming/应用数学原理/应用数学原理.md' },
+      { text: '计算机视觉', link: '/dezeming/计算机视觉/计算机视觉.md' },
+      { text: '计算机图形学', link: '/dezeming/计算机图形学/计算机图形学.md' },
     ]
   },
 ])
